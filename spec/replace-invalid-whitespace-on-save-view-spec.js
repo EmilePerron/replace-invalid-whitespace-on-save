f5e4cd4e83ec719bd3d1fb7edc460776a3bd0524
Émile Perron <contact@emileperron.com>
2019-03-06 12:26:15 -0500

    Removed spec directory and fixed issue caused by the package running on itself.

--- spec/replace-invalid-whitespace-on-save-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ReplaceInvalidWhitespaceOnSaveView from '../lib/replace-invalid-whitespace-on-save-view';
-
-describe('ReplaceInvalidWhitespaceOnSaveView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
